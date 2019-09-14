18efc19b517ae47b80ee0cb27add2c8a41bc26b4
Gareth Smith <gareth@totherme.org>
2019-09-14 15:47:49 +0100

    use proper parse->eval pattern

--- lib/parser_test.bash
+++ lib/parser_test.bash
@@ -14,5 +14,31 @@ T_parsing_number_writes_a_single_file() {
     parse "87" "${result_file}"
 
     read -r result <"${result_file}"
+    rm "${result_file}"
     [[ $result == "87" ]]
+}
+
+_parsing_a_simple_add_expression_writes_a_directory() {
+    tmpdir="$(dirname "$(mktemp -u)")"
+    result_path="$(mktemp "${tmpdir}/calc.XXXXXXXXXXXX.tmp")"
+
+    parse "2+8" "${result_path}"
+
+    if [[ ! -d ${result_path} ]] ; then
+	#shellcheck disable=2154
+	$T_fail "expected result path to be a directory"
+    fi
+    read -r lhs <"${result_file}/LHS"
+    if [[ $lhs != "2" ]] ; then
+	#shellcheck disable=2154
+	$T_fail "expected result LHS to be 2"
+    fi
+    read -r rhs <"${result_file}/RHS"
+    if [[ $rhs != "8" ]] ; then
+	#shellcheck disable=2154
+	$T_fail "expected result RHS to be 8"
+    fi
+
+    rm "${result_file}/LHS" "${result_file}/RHS"
+    rmdir "${result_file}"
 }
