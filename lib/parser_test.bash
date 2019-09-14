b033a45d92cf34f2959372aa90625fcd8851ca52
Gareth Smith <gareth@totherme.org>
2019-09-14 19:35:11 +0100

    use proper parse->eval pattern

--- lib/parser_test.bash
+++ lib/parser_test.bash
@@ -73,3 +73,49 @@ T_parsing_add_expressions_handles_whitespace() {
     rm "${result_path}/LHS" "${result_path}/operator" "${result_path}/RHS"
     rmdir "${result_path}"
 }
+
+_parsing_complex_add_expressions_works() {
+    tmpdir="$(dirname "$(mktemp -u)")"
+    result_path="$(mktemp "${tmpdir}/calc.XXXXXXXXXXXX.tmp")"
+
+    parse "2+8+37" "${result_path}"
+
+    read -r lhs <"${result_path}/LHS"
+    if [[ $lhs != "2" ]] ; then
+	#shellcheck disable=2154
+	$T_fail "expected result LHS to be 2"
+    fi
+    read -r op <"${result_path}/operator"
+    if [[ $op != "+" ]] ; then
+	#shellcheck disable=2154
+	$T_fail "expected result operator to be +"
+    fi
+
+    if [[ ! -d "${result_path}/RHS" ]] ; then
+	#shellcheck disable=2154
+	$T_fail "expected result RHS to be a directory"
+    fi
+    read -r rhs <"${result_path}/RHS/LHS"
+    if [[ $rhs != "8" ]] ; then
+	#shellcheck disable=2154
+	$T_fail "expected result RHS/LHS to be 8"
+    fi
+    read -r op <"${result_path}/RHS/operator"
+    if [[ $op != "+" ]] ; then
+	#shellcheck disable=2154
+	$T_fail "expected result RHS operator to be +"
+    fi
+    read -r rhs <"${result_path}/RHS/RHS"
+    if [[ $rhs != "37" ]] ; then
+	#shellcheck disable=2154
+	$T_fail "expected result RHS/RHS to be 37"
+    fi
+
+    rm "${result_path}/LHS" \
+       "${result_path}/operator" \
+       "${result_path}/RHS/LHS" \
+       "${result_path}/RHS/RHS" \
+       "${result_path}/RHS/operator" \
+       
+    rmdir "${result_path}/RHS" "${result_path}" 
+}
