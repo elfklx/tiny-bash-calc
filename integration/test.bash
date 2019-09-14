a15e80e42b8dd64046d67421b22433059ccbb953
Gareth Smith <gareth@totherme.org>
2019-09-14 09:19:39 +0100

    working

--- integration/test.bash
+++ integration/test.bash
@@ -4,7 +4,7 @@ set -euo pipefail
 
 [[ -z "${DEBUG:-""}" ]] || set -x
 
-_calc_reads_an_expression() {
+T_calc_reads_an_expression() {
     EXPR="8 + 3"
     RESULT=$(echo "$EXPR" | ./calc)
     [[ "$RESULT" =~ "11" ]]
