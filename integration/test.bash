e2b6d0464de10ee7874ce0822350edc86dec37fa
Gareth Smith <gareth@totherme.org>
2019-09-14 09:39:06 +0100

    working

--- integration/test.bash
+++ integration/test.bash
@@ -9,3 +9,13 @@ T_calc_evaluates_an_expression() {
     RESULT=$(echo -e "$EXPR" | ./calc)
     [[ "$RESULT" =~ "11" ]]
 }
+
+_calc_evaluates_multiple_expressions() {
+    EXPR="8 + 3\n12 - 5\nq"
+    RESULT=$(echo -e "$EXPR" | ./calc)
+    if [[ ! $RESULT =~ "11" ]] ; then
+	  # shellcheck disable=SC2154
+	$T_fail "first expression didn't evaluate"
+    fi
+    [[ "$RESULT" =~ "7" ]]
+}
