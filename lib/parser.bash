07500cc89caaaf1f6dd8c4095134c8eaaedf75d2
Gareth Smith <gareth@totherme.org>
2019-09-14 19:15:16 +0100

    use proper parse->eval pattern

--- lib/parser.bash
+++ lib/parser.bash
@@ -8,5 +8,18 @@ parse() {
     expr="${1:?expected first argument to parse to be an expression}"
     ast="${2:?expected second argument to parse to be a path where we write the result}"
 
-    echo "${expr}" > "${ast}"
+    # shelcheck disable=2076
+    if [[ "${expr}" =~ "+" ]] ; then
+	# add expression
+	lhs="${expr/+*}"
+	rhs="${expr/*+}"
+	if [[ -f "${ast}" ]] ; then rm "${ast}" ; fi
+	if [[ ! -d "${ast}" ]] ; then mkdir "${ast}" ; fi
+	echo "${lhs}" > "${ast}/LHS"
+	echo "${rhs}" > "${ast}/RHS"
+	echo "+" > "${ast}/operator"
+    else
+	# number expression    
+	echo "${expr}" > "${ast}"
+    fi
 }
