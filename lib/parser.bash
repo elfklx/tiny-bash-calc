b315f4228a17866d2cce137ef2f36979a7579985
Gareth Smith <gareth@totherme.org>
2019-09-14 15:18:27 +0100

    use proper parse->eval pattern

--- lib/parser.bash
+++ lib/parser.bash
@@ -4,3 +4,7 @@ set -euo pipefail
 
 [[ -z "${DEBUG:-""}" ]] || set -x
 
+parse() {
+    expr="${1:?expected first argument to parse to be an expression}"
+    ast="${2:?expected second argument to parse to be a path where we write the result}"
+}
--- lib/parser_test.bash
+++ lib/parser_test.bash
@@ -7,6 +7,17 @@ set -euo pipefail
 # shellcheck source=parser.bash
 . lib/parser.bash
 
+_parser_writes_to_output_file() {
+    tmpdir="$(dirname "$(mktemp -u)")"
+    result_file="$(mktemp "${tmpdir}/calc.XXXXXXXXXXXX.tmp")"
+
+    parse "xx" "${result_file}"
+
+    cat "${result_file}"
+    read -r result <"${result_file}"
+    [[ $result =~ "boo" ]]
+}
+
 _parsing_number_writes_a_single_file() {
     tmpdir="$(dirname "$(mktemp -u)")"
     result_file="$(mktemp "${tmpdir}/calc.XXXXXXXXXXXX.tmp")"
