bf5fe2e27c382baef6b93ec8797b75e028f1cc8b
Gareth Smith <gareth@totherme.org>
2019-09-15 21:17:02 +0100

    use proper parse->eval pattern

--- lib/parser_test.bash
+++ lib/parser_test.bash
@@ -100,11 +100,11 @@ _parsing_complex_add_expressions_works() {
     	#shellcheck disable=2154
     	$T_fail "expected result RHS/LHS to be 8"
     fi
-    # read -r op <"${result_path}/RHS/operator"
-    # if [[ $op != "+" ]] ; then
-    # 	#shellcheck disable=2154
-    # 	$T_fail "expected result RHS operator to be +"
-    # fi
+    read -r op <"${result_path}/RHS/operator"
+    if [[ $op != "+" ]] ; then
+    	#shellcheck disable=2154
+    	$T_fail "expected result RHS operator to be +"
+    fi
     # read -r rhs <"${result_path}/RHS/RHS"
     # if [[ $rhs != "37" ]] ; then
     # 	#shellcheck disable=2154
