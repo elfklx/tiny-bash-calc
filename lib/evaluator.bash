153fafa7ec6a1749bb9aac0359f3390f63666565
Gareth Smith <gareth@totherme.org>
2019-09-16 20:11:26 +0100

    use proper parse->eval pattern

---
+++ lib/evaluator.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+[[ -z "${DEBUG:-""}" ]] || set -x
+
+evaluate() {
+    true
+}
---
+++ lib/evaluator_test.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -u
+
+[[ -z "${DEBUG:-""}" ]] || set -x
+
+# shellcheck source=evaluator.bash
+. lib/evaluator.bash
+
+_evaluating_a_number_is_a_no_op() {
+    tmpdir="$(dirname "$(mktemp -u)")"
+    ast="$(mktemp "${tmpdir}/calc.XXXXXXXXXXXX.tmp")"
+    echo "32" > "${ast}"
+
+    evaluate "${ast}"
+
+    read -r result <"${ast}"
+    rm "${ast}"
+    [[ $result == "32" ]]
+}
