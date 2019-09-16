587c9ae094c58c166aef8dc648c4742e0aa3dd55
Gareth Smith <gareth@totherme.org>
2019-09-16 20:41:37 +0100

    use proper parse->eval pattern

--- lib/evaluator.bash
+++ lib/evaluator.bash
@@ -5,5 +5,17 @@ set -euo pipefail
 [[ -z "${DEBUG:-""}" ]] || set -x
 
 evaluate() {
-    true
+    local ast="${1:?Expected first argument to evaluate to be a path to an AST}"
+    if [[ -f "${ast}" ]] ; then
+	# already in normal form
+	return
+    fi
+
+    read -r lhs <"${ast}/LHS"
+    read -r rhs <"${ast}/RHS"
+    rm "${ast}/LHS"
+    rm "${ast}/RHS"
+    rm "${ast}/operator"
+    rmdir "${ast}"
+    echo "$(( lhs + rhs ))" > "${ast}"
 }
