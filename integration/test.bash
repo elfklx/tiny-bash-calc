04d69fe62f71b3b6ef26620e23bde1e0bec21f0f
Gareth Smith <gareth@totherme.org>
2019-09-17 20:11:50 +0100

    use proper parse->eval pattern

--- integration/test.bash
+++ integration/test.bash
@@ -8,7 +8,11 @@ T_calc_evaluates_an_expression() {
     local expr="8 + 3\nq"
     local result
     result=$(echo -e "$expr" | ./calc)
-    [[ "$result" =~ "11" ]]
+    if [[ ! "$result" =~ "11" ]] ; then
+	# shellcheck disable=SC2154
+	$T_fail "expected $result to give 11"
+	return
+    fi
 }
 
 T_calc_evaluates_multiple_expressions() {
@@ -16,7 +20,7 @@ T_calc_evaluates_multiple_expressions() {
     local result
     result=$(echo -e "$expr" | ./calc)
     if [[ ! $result =~ "11" ]] ; then
-	  # shellcheck disable=SC2154
+	# shellcheck disable=SC2154
 	$T_fail "first expression gave $result -- expected 11"
 	return
     fi
