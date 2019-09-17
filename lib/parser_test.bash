1d11b1a7c5ae668f54117d3c481e346542877e94
Gareth Smith <gareth@totherme.org>
2019-09-17 19:30:19 +0100

    use proper parse->eval pattern

--- lib/parser_test.bash
+++ lib/parser_test.bash
@@ -4,6 +4,8 @@ set -u
 
 [[ -z "${DEBUG:-""}" ]] || set -x
 
+# shellcheck source=test_helpers.bash
+. lib/test_helpers.bash
 # shellcheck source=parser.bash
 . lib/parser.bash
 
@@ -148,24 +150,3 @@ T_parsing_subtraction_writes_a_directory() {
 
     cleanup_tmpfile "${result_path}"    
 }
-
-setup_tmpfile() {
-    local tmpdir
-    tmpdir="$(dirname "$(mktemp -u)")"
-    mktemp "${tmpdir}/calc.XXXXXXXXXXXX.tmp"
-}
-
-cleanup_tmpfile() {
-    local tmpfile="${1:?Expected first arument to cleanup_tmpfile to be a tmpfile to clean up}"
-    local tmpdir
-    tmpdir="$(dirname "$(mktemp -u)")"
-
-    if [[ ! "$tmpfile" =~ ^$tmpdir ]] ; then
-	#shellcheck disable=2154
-	$T_fail "Can't clean up non-temporary files"
-	return
-    fi
-
-    rm -r "$tmpfile"
-}
-
