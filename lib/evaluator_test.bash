d62bf47b54e097cf6c1dcd3b3ab19bcf54a95fd6
Gareth Smith <gareth@totherme.org>
2019-09-18 20:55:14 +0100

    add multiplication

--- lib/evaluator_test.bash
+++ lib/evaluator_test.bash
@@ -33,19 +33,7 @@ T_evaluating_a_simple_subtraction_leaves_the_result() {
 }
 
 T_evaluating_a_simple_multiplication_leaves_the_result() {
-    local ast
-    ast="$(setup_tmpfile)"
-    rm "${ast}"
-    cp -r "fixtures/TwelveTimesThree" "${ast}"
-
-    evaluate "${ast}"
-
-    read -r result <"${ast}"
-    cleanup_tmpfile "${ast}"
-    if [[ $result != "36" ]] ; then
-	$T_fail "expected ${result} to equal 36"
-	return
-    fi
+    test_fixture_evaluation "fixtures/TwelveTimesThree" 36
 }
 
 test_fixture_evaluation() {
