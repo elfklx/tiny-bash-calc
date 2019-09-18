bf85cad780b5443c732443045d37c632f6f6f7fd
Gareth Smith <gareth@totherme.org>
2019-09-18 19:43:12 +0100

    add multiplication

--- lib/parser.bash
+++ lib/parser.bash
@@ -17,8 +17,8 @@ parse() {
 	# add expression
 	parse_binexp "+" "${expr}" "${ast}"
     elif [[ "${expr}" =~ "*" ]] ; then
-	# add expression
-	parse_binexp "*" "${expr}" "${ast}"
+	# multiplication expression
+	parse_mult "${expr}" "${ast}"
     else
 	# number expression    
 	echo "${expr}" > "${ast}"
@@ -39,6 +39,21 @@ parse_binexp() {
     parse "${lhs}" "${ast}/LHS"
     parse "${rhs}" "${ast}/RHS"
     echo "${op}" > "${ast}/operator"    
+}
+
+parse_mult() {
+    local expr="${1:?expected first argument of parse_mult to be an expression}"
+    local ast="${2:?expected second argument of parse_mult to be a path where we write the result}"
+
+    local lhs rhs
+    lhs="${expr/\**}"
+    rhs="${expr#${lhs}*\*}"
+
+    if [[ ! -d "${ast}" ]] ; then mkdir "${ast}" ; fi
+
+    parse "${lhs}" "${ast}/LHS"
+    parse "${rhs}" "${ast}/RHS"
+    echo "\*" > "${ast}/operator"    
 }
 
 get_lhs() {
