6442e3e88dbb72f13940320ceb2542ddceb6a2bf
Gareth Smith <gareth@totherme.org>
2019-09-18 20:47:21 +0100

    add multiplication

--- lib/evaluator_test.bash
+++ lib/evaluator_test.bash
@@ -71,3 +71,21 @@ T_evaluating_a_simple_multiplication_leaves_the_result() {
 	return
     fi
 }
+
+test_fixture_evaluation() {
+    local ast fixture_path expected_result
+    fixture_path="${1:?first argument to test_fixture_evaluation should be a fixture}"
+    expected_result="${1:?second argument to test_fixture_evaluation should be our expected result}"
+    ast="$(setup_tmpfile)"
+    rm "${ast}"
+    cp -r "${fixture_path}" "${ast}"
+
+    evaluate "${ast}"
+
+    read -r result <"${ast}"
+    cleanup_tmpfile "${ast}"
+    if [[ $result != "${expected_result}" ]] ; then
+	$T_fail "expected ${result} to equal ${expected_result}"
+	return
+    fi
+}
