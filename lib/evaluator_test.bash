0f4723b3c03ddc3a57bbc8275aca1352719b50d2
Gareth Smith <gareth@totherme.org>
2019-10-19 18:06:07 +0100

    Make complex expressions work end-to-end

--- lib/evaluator_test.bash
+++ lib/evaluator_test.bash
@@ -36,6 +36,10 @@ T_evaluating_a_simple_multiplication_leaves_the_result() {
     test_fixture_evaluation "fixtures/TwelveTimesThree" 36
 }
 
+_evaluating_a_complex_expression_leaves_the_result() {
+    test_fixture_evaluation "fixtures/OnePlusTwoTimesThree" 36
+}
+
 test_fixture_evaluation() {
     local ast fixture_path expected_result
     fixture_path="${1:?first argument to test_fixture_evaluation should be a fixture}"
